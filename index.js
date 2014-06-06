014d4ac238f6d898e7c0dbd6f41696d0e94f62fc
Ryan Smith <r@32k.io>
2014-06-06 10:22:48 -0700

    init

---
+++ index.js
@@ -0,0 +1,51 @@
+var request = require('request');
+var fs = require("fs");
+
+var URL = "https://api.chain.com";
+var PEM = fs.readFileSync("./chain.pem");
+var KEY = "GUEST-TOKEN";
+var OPT = {
+  host: URL,
+  strictSSL: true,
+  cert: PEM,
+  auth: {user: KEY}
+};
+
+module.exports = {
+  getAddress: function(addr, cb) {
+    request({
+      method: 'GET',
+      uri: OPT.host + '/v1/bitcoin/addresses/' + addr,
+      strictSSL: OPT.strictSSL,
+      cert: OPT.cert,
+      auth: OPT.auth
+    }, function(err, msg, resp) {
+      cb(err, resp);
+    });
+  },
+
+  sendTransaction: function(txnHex) {
+    request({
+      method: 'PUT',
+      uri: OPT.host + '/v1/bitcoin/transactions/',
+      strictSSL: OPT.strictSSL,
+      cert: OPT.cert,
+      auth: OPT.auth,
+      json: {hex: txnHex},
+    }, function(err, msg, resp) {
+      cb(err, resp);
+    });
+  },
+
+  getUnspentOutputs: function(addr) {
+    request({
+      method: 'GET',
+      uri: OPT.host + '/v1/bitcoin/addresses/' + addr + '/unspents',
+      strictSSL: OPT.strictSSL,
+      cert: OPT.cert,
+      auth: OPT.auth,
+    }, function(err, msg, resp) {
+      cb(err, resp);
+    });
+  }
+};
