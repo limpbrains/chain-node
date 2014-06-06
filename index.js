5688966a691fffa53e2e26faaff16807450d3ca8
Ryan Smith <r@32k.io>
2014-06-06 10:56:41 -0700

    bugfix: allow key override mechanism

--- index.js
+++ index.js
@@ -5,21 +5,15 @@ var path = require("path");
 var URL = "https://api.chain.com";
 var PEM = fs.readFileSync(path.join(__dirname, "./chain.pem"));
 var KEY = "GUEST-TOKEN";
-var OPT = {
-  host: URL,
-  strictSSL: true,
-  cert: PEM,
-  auth: {user: KEY}
-};
 
 module.exports = {
   getAddress: function(addr, cb) {
     request({
       method: 'GET',
-      uri: OPT.host + '/v1/bitcoin/addresses/' + addr,
-      strictSSL: OPT.strictSSL,
-      cert: OPT.cert,
-      auth: OPT.auth
+      uri: URL + '/v1/bitcoin/addresses/' + addr,
+      strictSSL: true,
+      cert: PEM,
+      auth: {user: KEY},
     }, function(err, msg, resp) {
       cb(err, resp);
     });
@@ -28,10 +22,10 @@ module.exports = {
   sendTransaction: function(txnHex, cb) {
     request({
       method: 'PUT',
-      uri: OPT.host + '/v1/bitcoin/transactions/',
-      strictSSL: OPT.strictSSL,
-      cert: OPT.cert,
-      auth: OPT.auth,
+      uri: URL + '/v1/bitcoin/transactions/',
+      strictSSL: true,
+      cert: PEM,
+      auth: {user: KEY},
       json: {hex: txnHex},
     }, function(err, msg, resp) {
       cb(err, resp);
@@ -41,10 +35,10 @@ module.exports = {
   getUnspentOutputs: function(addr, cb) {
     request({
       method: 'GET',
-      uri: OPT.host + '/v1/bitcoin/addresses/' + addr + '/unspents',
-      strictSSL: OPT.strictSSL,
-      cert: OPT.cert,
-      auth: OPT.auth,
+      uri: URL + '/v1/bitcoin/addresses/' + addr + '/unspents',
+      strictSSL: true,
+      cert: PEM,
+      auth: {user: KEY},
     }, function(err, msg, resp) {
       cb(err, resp);
     });
